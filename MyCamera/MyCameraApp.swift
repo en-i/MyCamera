90a09b852a2b4ee3b9a6b882c49a9aa38ead8b19
en-i <templebond@icloud.com>
2021-07-10 23:38:46 +0900

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by terada enishi on 2021/07/10.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
